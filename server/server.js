10328fe4fd2e77a84e23113ddf4ca69a61e4a9ad
Dare Ololade <oladare186@gmail.com>
2026-04-13 15:54:30 +0100

    fix cors and client url

--- server/server.js
+++ server/server.js
@@ -10,17 +10,18 @@ dotenv.config();
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
+const port = process.env.PORT || 5000;
+const CLIENT_URL = process.env.CLIENT_URL;
+
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: CLIENT_URL,
     methods: ["GET", "POST"],
   },
 });
 
 connectDB();
-const port = process.env.PORT || 5000;
-const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 
 app.use(
   cors({
